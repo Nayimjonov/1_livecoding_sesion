34c609d935e3b42af48713c64b7617100dee86a9
Samandar <samandarnayimjonov@gmail.com>
2025-03-06 15:17:03 +0500

    tag adminka yozildi

--- authors/models.py
+++ authors/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
